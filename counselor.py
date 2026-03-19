6e1037fb93c69af85283d5ddd2b9432298406dc9
fatmasahli740-cpu <fatmasahli740@gmail.com>
2026-03-19 17:00:29 +0100

    Update counselor.py

--- counselor.py
+++ counselor.py
@@ -38,7 +38,7 @@ if prompt := st.chat_input("How can I help you today?"):
             full_response = ""
             
             # Request streaming completion
-# Added the closing parenthesis ')' at the end
+
             completion = client.chat.completions.create(
                 model=model,
                 messages=st.session_state.messages,
